dc1d23db81f5d5cd604e637565a981efd7f3e69c
Christine Lo <christine.lo.hayes@gmail.com>
2021-09-22 21:59:06 -0400

    added constants

--- client.js
+++ client.js
@@ -1,11 +1,12 @@
 const net = require('net');
+const { IP, PORT } = require("./constants");
 
 // establishes a connection with the game server
 const connect = function() {
   
   const conn = net.createConnection({
-    host: '135.23.223.133',
-    port: 50542
+    host: IP,
+    port: PORT
   });
 
   // interpret incoming data as text
@@ -17,7 +18,7 @@ const connect = function() {
  
   conn.on('connect', () => {
     console.log("Successfully connected to game server");
-    conn.write('Name: CLO');  
+    conn.write('Name: CLO');
   });
 
   return conn;
