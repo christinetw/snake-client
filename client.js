2f03a7e3f9cb4ab90b9ac81ca8e4cde88dc4c710
Christine Lo <christine.lo.hayes@gmail.com>
2021-09-22 21:48:42 -0400

    send keyboard actions to server

--- client.js
+++ client.js
@@ -1,7 +1,7 @@
 const net = require('net');
 
 // establishes a connection with the game server
-const connect = function () {
+const connect = function() {
   
   const conn = net.createConnection({
     host: '135.23.223.133',
